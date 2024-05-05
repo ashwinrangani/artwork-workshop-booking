05f65279726f7894290150ee1cb7a2a02da73da2
ashwin_rangani <ashwyn.rangani@gmail.com>
2024-05-06 00:24:08 +0530

    image popup

--- artwork-frontend/src/components/Header.jsx
+++ artwork-frontend/src/components/Header.jsx
@@ -1,6 +1,19 @@
+import { useState, Fragment } from 'react';
 import { Slide, Fade, Zoom } from 'react-awesome-reveal';
+import { Dialog, Transition } from "@headlessui/react";
 
 const Header = () => {
+
+  let [isOpen, setIsOpen] = useState(false)
+
+  function closeModal() {
+    setIsOpen(false)
+  }
+
+  function openModal() {
+    setIsOpen(true)
+  }
+
   return (
     <div className="relative isolate overflow-hidden bg-gray-900 py-24 sm:py-32">
       
@@ -36,8 +49,10 @@ const Header = () => {
       <div className="absolute right-auto mt-2  md:right-16 lg:right-48 ">
         <Zoom duration={2000}>
         <img
-          className="w-16 h-16 ml-2 md:w-36 md:h-36 lg:w-52 lg:h-52 border-2 border-gray-400 hover:border-gray-500 md:border-4 rounded-full"
+          className="w-16 h-16 cursor-pointer ml-2 md:w-36 md:h-36 lg:w-52 lg:h-52 border-2 border-gray-400 hover:border-gray-500 md:border-4 rounded-full"
           src="./my-image2.jpg"
+          onClick={openModal}
+          
         /></Zoom>
       </div>
       <div className="mx-auto max-w-7xl px-6 lg:px-8">
@@ -93,6 +108,46 @@ const Header = () => {
         </div>
         
       </div>
+      { isOpen && <Transition appear show={isOpen} as={Fragment}>
+        <Dialog as="div" className="relative z-10" onClose={closeModal}>
+          <Transition.Child
+            as={Fragment}
+            enter="ease-out duration-300"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+            leave="ease-in duration-200"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            <div className="fixed inset-0 bg-black/75" />
+          </Transition.Child>
+
+          <div className="fixed inset-0 overflow-y-auto">
+            <div className="flex min-h-full items-center justify-center p-4 text-center">
+              <Transition.Child
+                as={Fragment}
+                enter="ease-out duration-300"
+                enterFrom="opacity-0 scale-95"
+                enterTo="opacity-100 scale-100"
+                leave="ease-in duration-200"
+                leaveFrom="opacity-100 scale-100"
+                leaveTo="opacity-0 scale-95"
+              >
+                <Dialog.Panel className="w-full max-w-md transform overflow-hidden rounded-2xl  p-6 text-left align-middle shadow-xl transition-all">
+                  
+                  <div>
+                    <img src='/my-image2.jpg' className='rounded-full' />
+                  </div>
+
+                  
+                </Dialog.Panel>
+              </Transition.Child>
+            </div>
+          </div>
+        </Dialog>
+      </Transition> }
+      
+      
     </div>
   );
 };
